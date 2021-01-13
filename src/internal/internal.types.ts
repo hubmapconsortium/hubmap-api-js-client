22684c1dbe7c5a1e7c9d9e312a3b5e3df17ed85c
John Conroy <john-conroy@users.noreply.github.com>
2021-01-13 10:36:53 -0500

    Modify types

--- src/internal/internal.types.ts
+++ src/internal/internal.types.ts
@@ -8,9 +8,18 @@ interface InputTypes {
 interface CheckParametersTypes {
   inputType: string;
   outputType: keyof InputTypes;
-  // inputSet: string[];
-  genomicModality: string;
+  inputSet?: string[];
+  genomicModality?: string;
   pValue?: number;
 }
+interface RequestObjectTypes {
+  /* eslint-disable camelcase */
+  input_type: string;
+  input_set: string[];
+  genomic_modality?: string;
+  p_value?: number;
+  logical_operator: 'and';
+  /* eslint-enable camelcase */
+}
 
-export { InputTypes, CheckParametersTypes };
+export { InputTypes, CheckParametersTypes, RequestObjectTypes };
